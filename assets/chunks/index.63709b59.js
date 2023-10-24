f51a08a584fcb7e7c250f685cf28f69c01f8e549
fangzhioo <fangzhioo@users.noreply.github.com>
2023-10-24 01:30:02 +0000

    deploy: 982feff51944ff2520c44915048f4ae126f00901

---
+++ assets/chunks/index.63709b59.js
@@ -0,0 +1,1 @@
+const s=t=>(t.install=o=>{const{name:n}=t;n&&o.component(n,t)},t);export{s as i};
---
+++ assets/chunks/use-prop.a1bc5091.js
@@ -0,0 +1,1 @@
+import{g as t,a2 as p}from"./framework.0391d42f.js";const a=r=>{var o;const s=(o=p().proxy)==null?void 0:o.$props;return t(()=>s[r]??void 0)};export{a as u};
